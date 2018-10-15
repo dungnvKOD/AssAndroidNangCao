cd5d9782fca200c714e06c570c2f29458346621e
Dung <dung06011995@gmail.com>
2018-10-15 23:45:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dung/ass/map/MapActivity.kt
@@ -0,0 +1,15 @@
+package com.example.dung.ass.map
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dung.ass.R
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+        
+
+    }
+}
